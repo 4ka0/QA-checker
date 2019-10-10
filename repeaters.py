96c8768e9b132a361a0292ccb5f4fa40704e0edd
4ka0 <jage@fastmail.com>
2019-10-10 19:59:29 +0100

    Update repeaters.py

--- repeaters.py
+++ repeaters.py
@@ -14,30 +14,36 @@ def repeated_word_check(segments):
     Loop for catching single word repetitions such as "the the".
     '''
     for segment in segments:
+
+        print("\nCurrent list of repeated words=")
+        print(segment.repeated_words)
         
         # Only proceed if there is English text in the segment.
         if segment.eng_text:
-            substrings = segment.eng_text.split()
             previous_substring = ""
-        
+            substrings = segment.eng_text.split()
+
             for substring in substrings:
                 # clean punctuation chars etc. from substring
                 current_string = re.sub('[,.;:/?*"+=!_@#$<>()\[\]]', '', substring)
         
                 if previous_substring.lower() == current_string.lower():
-                    # print("... MATCH FOUND")
-                    # print(previous_substring.lower())
-                    # print(current_string.lower())
-
+ 
+                    print("... MATCH FOUND")
+                    print(previous_substring.lower())
+                    print(current_string.lower())
                     '''
                     !!! Bug found !!!
                     REPEATED WORD FOUND.
 					['is is', 'circuit circuit']
+					segment.repeated_words is somehow GLOBAL !?
 					'''
-                    segment.repeated_words.append(previous_substring + " " + current_string)
-                    # print(segment.repeated_words)
-                    segment.repeated_word = True
                     segment.error_found = True
+                    segment.repeated_word = True
+                    segment.repeated_words.append(previous_substring + " " + current_string)
+                    print("New list of repeated words =")
+                    print(segment.repeated_words)
+
                 previous_substring = current_string
 
     '''
