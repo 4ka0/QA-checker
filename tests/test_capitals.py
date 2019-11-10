0621ed158e2bbc5a54c8c9b04d9c9411e0ab308d
4ka0 <jage@fastmail.com>
2019-11-10 22:33:27 +0000

    Test update

--- tests/test_capitals.py
+++ tests/test_capitals.py
@@ -1,4 +1,20 @@
-from checker import capitals
+import pytest
+from checker.gather import Segment
+import checker.capitals
 
-def test_leading_capital_check():
+'''
+Need to create a fixture of a list
+of Segment objects, each object including
+some Japanese and English text.
+'''
+
+
+@pytest.fixture
+def segment():
+    return Segment('', '', [], [], False, {}, {},
+                   False, False, False, False, False, False,
+                   [], False, [], [], [], {}, {}, False, False, [])
+
+
+def test_leading_capital_check(segment):
     pass
